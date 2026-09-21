8a0ba26fed2b9f529c6f97bdf689cdd8fcd51113
FVK4484 <180932485+FVK4484@users.noreply.github.com>
2026-09-21 19:32:47 +0200

    P1 Ejercicio1.

--- Laboratorios/PI12627Base/src/ejercicios/Ejercicio1.java
+++ Laboratorios/PI12627Base/src/ejercicios/Ejercicio1.java
@@ -1,6 +1,11 @@
 package ejercicios;
 
+import java.security.spec.ECField;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 
@@ -8,7 +13,6 @@ public class Ejercicio1 {
 	
 	public static void tests(List<ParEnteros> ls) {
 		ls.forEach(t -> {
-			System.out.println();
 			System.out.println("1) Solucion Funcional: " + funcional(t.a(), t.b()));
 			System.out.println("2) Solucion Iterativo: " + iterativo(t.a(), t.b()));
 			System.out.println("3) Solucion Rec. Final: " + recFinal(t.a(), t.b()));
@@ -16,20 +20,66 @@ public class Ejercicio1 {
 		
 	}
 	
+	public static String funcional(Integer varA, Integer varB) {
+		
+		UnaryOperator<EnteroCadena> nx = elem -> { 
+			return EnteroCadena.of(elem.a() + 3, elem.a() % 2 == 0 ? elem.a() + "*" : elem.a() + "!");
+			
+		};
+		
+		return Stream.iterate(EnteroCadena.of(varA,"A"), 
+				elem -> elem.a() < varB, nx)
+				.filter(elem -> elem.a() % 10 != 0)
+				.map(elem -> elem.s())
+				.collect(Collectors.joining("-"));
+		
+		} 
 	
-	public static String funcional(Integer varA, Integer varB) {	
-		return null;
-	} 
-
-	
-	public static String iterativo(Integer varA, Integer varB) { 
-		return null;
+	public static String iterativo(Integer varA, Integer varB) {
+		
+	    List<String> res = new ArrayList<>();
+	    EnteroCadena elem = EnteroCadena.of(varA, "A");
+	    
+	    while (elem.a() < varB) {
+	        if (elem.a() % 10 != 0) {
+	            res.add(elem.s());
+	        }
+	        
+	        int nextA = elem.a() + 3;
+	        String nextS = (elem.a() % 2 == 0) ? elem.a() + "*" : elem.a() + "!";
+	        elem = EnteroCadena.of(nextA, nextS);
+	        
+	    }
+	    
+	    return String.join("-", res);
+	    
 	}
 	
 	public static String recFinal(Integer varA, Integer varB) {
-		return null;
+		
+		return recFinal(varA, varB, new EnteroCadena(varA, "A"), "");
+		
 	}
 	
-
+	private static String recFinal(Integer varA, Integer varB, EnteroCadena elem, String ac) {
+		
+		if (elem.a() < varB) {
+			
+			if (elem.a() % 10 != 0) {
+				
+				ac = ac.isEmpty() ? elem.s() : ac + "-" + elem.s();;
+	            
+	        }
+			
+        	int nextA = elem.a() + 3;
+	        String nextS = (elem.a() % 2 == 0) ? elem.a() + "*" : elem.a() + "!";
+	        elem = EnteroCadena.of(nextA, nextS);
+	        return recFinal(nextA, varB, elem, ac);
+	        
+		}
+		
+		return ac;
+		
+	}
 
 }
